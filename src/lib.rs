5a08394b6c063c80c4655b4309dbb04b141b06ce
emanuel-sol <esolis6114@gmail.com>
2024-03-22 17:32:53 -0400

    refactor(lib.rs): Refactor run method to return a Server
    run method returns a Server so we can pass it to tokio::spawn in our test logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-// It is no longer the binary entrypoint, therefore we can mark it
-// as async without using the #[tokio::main] proc-macro
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+
+    Ok(server)
 }
