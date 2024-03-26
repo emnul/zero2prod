add56f416534704d801baa98efb378e1e711d63e
emanuel-sol <esolis6114@gmail.com>
2024-03-25 22:39:15 -0400

    refactor(lib.rs): Split lib.rs out into several different files
    Refactors core functionality into seperate modules
    
    BREAKING CHANGE: Need to update imports in some files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::startup;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0")?;
-    run(listener)?.await
+    startup::run(listener)?.await
 }
---
+++ src/startup.rs
@@ -0,0 +1,18 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use std::net::TcpListener;
+
+use crate::routes::health_check;
+use crate::routes::subscriptions;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check::health_check))
+            .route("/subscriptions", web::post().to(subscriptions::subscribe))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
