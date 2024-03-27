d8865ceb3054fc4ea92d75a1c79fa4093afd4421
emanuel-sol <esolis6114@gmail.com>
2024-03-27 18:20:56 -0400

    feat(src/main.rs): Use tracing-log crate to redirect `log`'s events to our subscriber
    `log` does not emit tracing events out of the box. Thus, we will not see logs emitted by actix-web without it.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Redirect all `log`'s events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new(
         "zero2prod".into(),
