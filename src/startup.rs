708ac1243979197316d18407a3a87a12afbb94b0
emanuel-sol <esolis6114@gmail.com>
2024-03-27 15:44:39 -0400

    feat(src/startup.rs): Add Logger middleware to application

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Get a pointer copy and attach it to the application state
