be3c3c84ada7e509031cad841438fc20732ac859
emanuel-sol <esolis6114@gmail.com>
2024-03-26 21:22:29 -0400

    feat(src/startup.rs): Register a postgres connection as part of application state
    To execute a query in subscribe method we need a database connection
    
    BREAKING CHANGE: Need to refector main.rs

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,19 @@
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-use crate::routes::health_check;
-use crate::routes::subscriptions;
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
-            .route("/health_check", web::get().to(health_check::health_check))
-            .route("/subscriptions", web::post().to(subscriptions::subscribe))
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
