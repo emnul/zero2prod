50913fbda52d6b910833cf6685b48ebcfe831d6c
emanuel-sol <esolis6114@gmail.com>
2024-03-28 17:05:03 -0400

    refactor(src/routes/subscriptions): Cleans up instrumentation code using the #[tracing::instrument] proc macroand seperates concerns of the subscribe function
    Subscribe now just orchestrates work to be done while insert_subscriber takes care of db logic

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,49 +10,47 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     // Retrieve connection from application state
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-      "Adding a new subscriber",
-      %request_id,
-      subscriber_email = %form.email,
-      subscriber_name = %form.name,
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    );
-    let _request_span_guard = request_span.enter();
-
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
-        "#,
+      INSERT INTO subscriptions (id, email, name, subscribed_at)
+      VALUES ($1, $2, $3, $4)
+      "#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    // We use `get_ref` to get an immutable reference to the `PgConnection`
-    // wrapped by `web::Data`
-    .execute(pool.get_ref())
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!("request_id {} - Failed to execute query: {}", request_id, e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
